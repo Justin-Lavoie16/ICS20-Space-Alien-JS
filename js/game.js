f9879ca17cf3a6e3d51e4cd746c495f7400554b7
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-11-10 19:11:05 +0000

    Prettified Code!

--- js/game.js
+++ js/game.js
@@ -1,19 +1,18 @@
 const config = {
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  physics: {
-    default: "arcade",
-    arcade: {
-      debug: true,
+    type: Phaser.AUTO,
+    width: 1920,
+    height: 1080,
+    physics: {
+      default: "arcade",
+      arcade: {
+        debug: true,
+      },
+    },
+    backgroundColor: 0x5f6e7a,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
     },
   },
- backgroundColor: 0x5f6e7a,
-      scale: {
-        mode: Phaser.Scale.FIT,
-         autoCenter: Phaser.Scale.CENTER_BOTH,
-      },
- },
-
-game = new Phaser.Game(config)
+  game = new Phaser.Game(config)
 console.log(game)
