78b9bace95c1913bb2e052c5395b73534e3c2079
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-10 14:07:27 -0500

    453

--- js/game.js
+++ js/game.js
@@ -2,8 +2,18 @@ const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a,
-}
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+ backgroundColor: 0x5f6e7a,
+      scale: {
+        mode: Phaser.Scale.FIT,
+         autoCenter: Phaser.Scale.CENTER_BOTH,
+      },
+ },
 
-const game = new Phaser.Game(config)
+game = new Phaser.Game(config)
 console.log(game)
