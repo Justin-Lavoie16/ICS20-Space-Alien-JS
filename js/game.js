b3626c181adb593634bc95d16087fddc7fe25c8e
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-15 13:52:10 -0500

    32

--- js/game.js
+++ js/game.js
@@ -1,18 +1,25 @@
+import SplashScene from "./splashScene.js"
+
+const splashScene = new SplashScene()
+
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    physics: {
-      default: "arcade",
-      arcade: {
-        debug: true,
-      },
-    },
-    backgroundColor: 0x5f6e7a,
-    scale: {
-      mode: Phaser.Scale.FIT,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
   },
-  game = new Phaser.Game(config)
+ backgroundColor: 0x5f6e7a,
+      scale: {
+        mode: Phaser.Scale.FIT,
+         autoCenter: Phaser.Scale.CENTER_BOTH,
+      },
+ },
+
+ game = new Phaser.Game(config)
 console.log(game)
+
+game.scene.add("splashScene", splashScene)
