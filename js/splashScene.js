cebb580ad06ff2fc0b219b9faacc7268175b84bc
Justin-Lavoie16 <Justin-Lavoie16@users.noreply.github.com>
2022-11-15 19:07:41 +0000

    Prettified Code!

--- js/splashScene.js
+++ js/splashScene.js
@@ -1,27 +1,21 @@
-
 class SplashScene extends Phaser.Scene {
-
   constructor() {
     super({ key: "splashScene" })
 
     this.splashSceneBackgroundImage = null
   }
 
-
   init(data) {
     this.cameras.main.setBackgroundColor("#ffffff")
   }
-
 
   preload() {
     console.log("Splash Scene")
   }
 
-  create(data) {
-  }
+  create(data) {}
 
-  update(time, delta) {
-  }
+  update(time, delta) {}
 }
 
 export default SplashScene
