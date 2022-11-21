7c90db76d7d223c8372937bcb3765c1c8a0e5c14
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-21 10:01:38 -0500

    23

--- js/game.js
+++ js/game.js
@@ -1,24 +1,31 @@
 import SplashScene from "./splashScene.js"
+import TitleScene from './titleScene.js'
+
 
 const splashScene = new SplashScene()
+const titleScene = new TitleScene()
 
 const config = {
-    type: Phaser.AUTO,
-    width: 1920,
-    height: 1080,
-    physics: {
-      default: "arcade",
-      arcade: {
-        debug: true,
-      },
-    },
-    backgroundColor: 0x5f6e7a,
-    scale: {
-      mode: Phaser.Scale.FIT,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
     },
   },
-  game = new Phaser.Game(config)
+ backgroundColor: 0x5f6e7a,
+      scale: {
+        mode: Phaser.Scale.FIT,
+         autoCenter: Phaser.Scale.CENTER_BOTH,
+      },
+},
+
+const game = new Phaser.Game(config)
 console.log(game)
 
 game.scene.add("splashScene", splashScene)
+game.scene.add('titleScene', titleScene)
+
+game.scene.start('splashScene')
--- js/splashScene.js
+++ js/splashScene.js
@@ -1,21 +1,29 @@
+
 class SplashScene extends Phaser.Scene {
+
   constructor() {
     super({ key: "splashScene" })
 
     this.splashSceneBackgroundImage = null
   }
 
+
   init(data) {
     this.cameras.main.setBackgroundColor("#ffffff")
   }
+
 
   preload() {
     console.log("Splash Scene")
   }
 
-  create(data) {}
+  create(data) {
+  }
 
-  update(time, delta) {}
+update (time, delta) {
+    if (time > 3000) {
+      this.scene.switch('titleScene')
+  }
 }
 
 export default SplashScene
