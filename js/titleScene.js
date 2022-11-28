2bdbc0552080435def0aeb055cd4133d50398a9c
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-28 14:05:18 -0500

    543

--- js/titleScene.js
+++ js/titleScene.js
@@ -1,19 +1,40 @@
 class TitleScene extends Phaser.Scene {
   constructor() {
-    super({ key: "titleScene" })
-  }
+   super({ key: "titleScene" })
 
-  init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
-  }
+   this.titleSceneBackgroundImage = null
+   this.titleSceneText = null
+   this.titleSceneTextStyle = {
+     font: "200px Times",
+     fill: "#fde4b9",
+     align: "center",
+   }
+ }
 
-  preload() {
-    console.log("Title Scene")
-  }
 
-  create(data) {}
+init (data) {
+   this.cameras.main.setBackgroundColor('#ffffff')
+ }
 
-  update(time, delta) {}
+preload () {
+   console.log('Title Scene')
+ this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
+ }
+
+create(data) {
+   this.titleSceneBackgroundImage = this.add
+     .sprite(0, 0, "titleSceneBackground")
+     .setScale(2.75)
+   this.titleSceneBackgroundImage.x = 1920 / 2
+   this.titleSceneBackgroundImage.y = 1080 / 2
+
+   this.titleSceneText = this.add
+     .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+     .setOrigin(0.5)
+ }
+
+update (time, delta) {
+ }
 }
 
 export default TitleScene
