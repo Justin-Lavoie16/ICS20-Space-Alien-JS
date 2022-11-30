63923fe172e884ff9c944ceb1ac911096661efc8
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-30 13:45:05 -0500

    re

--- js/gameScene.js
+++ js/gameScene.js
@@ -1,19 +1,33 @@
 class GameScene extends Phaser.Scene {
+
+
   constructor() {
-    super({ key: "gameScene" })
-  }
-
-  init(data) {
-    this.cameras.main.setBackgroundColor("ffffff")
-  }
-
-  preload() {
-    console.log("Game Scene")
-  }
-
-  create(data) {}
-
-  update(time, delta) {}
-}
-
-export default GameScene
+     super({ key: "gameScene" })
+ 
+     this.background = null
+     this.ship = null
+   }
+ 
+ init(data) {
+     this.cameras.main.setBackgroundColor("ffffff")
+   }
+ 
+   preload() {
+     console.log("Game Scene")
+ 
+     this.load.image("starBackground", "./assets/starBackground.png")
+     this.load.image("ship", "./assets/spaceShip.png")
+   }
+ 
+   create(data) {
+     this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+     this.background.setOrigin(0, 0)
+ 
+     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
+   }
+ 
+  update(time, delta) {
+       }
+     }
+ 
+ export default GameScene
