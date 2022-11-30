480d976516935b20c6e8c7d75fd133205c1f03ff
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-11-29 20:20:41 -0500

    rew

---
+++ js/gameScene.js
@@ -0,0 +1,23 @@
+class GameScene extends Phaser.Scene {
+
+
+    constructor() {
+      super({ key: "gameScene" })
+    }
+  
+  init(data) {
+      this.cameras.main.setBackgroundColor("ffffff")
+    }
+  
+   preload() {
+      console.log("Game Scene")
+    }
+  
+    create(data) {
+    }
+  
+   update(time, delta) {
+        }
+      }
+  
+  export default GameScene
