168c5d8b9a580ebe151bfad5c27aa1eab89b3703
Justin-Lavoie16 <113055699+Justin-Lavoie16@users.noreply.github.com>
2022-12-12 14:24:34 -0500

    3123

--- js/titleScene.js
+++ js/titleScene.js
@@ -1,42 +1,43 @@
 class TitleScene extends Phaser.Scene {
   constructor() {
-    super({ key: "titleScene" })
+   super({ key: "titleScene" })
 
-    this.titleSceneBackgroundImage = null
-    this.titleSceneText = null
-    this.titleSceneTextStyle = {
-      font: "200px Times",
-      fill: "#fde4b9",
-      align: "center",
-    }
-  }
+   this.titleSceneBackgroundImage = null
+   this.titleSceneText = null
+   this.titleSceneTextStyle = {
+     font: "200px Times",
+     fill: "#fde4b9",
+     align: "center",
+   }
+ }
 
-  init(data) {
-    this.cameras.main.setBackgroundColor("#ffffff")
-  }
 
-  preload() {
-    console.log("Title Scene")
-    this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
-  }
+init (data) {
+   this.cameras.main.setBackgroundColor('#ffffff')
+ }
 
-  create(data) {
-    this.titleSceneBackgroundImage = this.add
-      .sprite(0, 0, "titleSceneBackground")
-      .setScale(2.75)
-    this.titleSceneBackgroundImage.x = 1920 / 2
-    this.titleSceneBackgroundImage.y = 1080 / 2
+preload () {
+   console.log('Title Scene')
+ this.load.image("titleSceneBackground", "assets/aliens_screen_image.jpg")
+ }
 
-    this.titleSceneText = this.add
-      .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
-      .setOrigin(0.5)
-  }
+create(data) {
+   this.titleSceneBackgroundImage = this.add
+     .sprite(0, 0, "titleSceneBackground")
+     .setScale(2.75)
+   this.titleSceneBackgroundImage.x = 1920 / 2
+   this.titleSceneBackgroundImage.y = 1080 / 2
 
-  update(time, delta) {
-    if (time > 6000) {
-      this.scene.switch("menuScene")
-    }
-  }
+   this.titleSceneText = this.add
+     .text(1920 / 2, 1080 / 2 + 350, "Space Aliens", this.titleSceneTextStyle)
+     .setOrigin(0.5)
+ }
+
+update (time, delta) {
+ if (time > 6000) {
+   this.scene.switch('menuScene')
+ }
+ }
 }
 
 export default TitleScene
